e847c261ead4b56b74a5e2b9e58b572ca5119083
DPF0190 <domenick.fumai@gmail.com>
2016-07-28 22:54:54 +0000

    mymixin

--- mymixin.py
+++ mymixin.py
@@ -2,6 +2,8 @@ from json import loads, dumps
 
 class MySerializer(object):
 
+    jsonstr = {'alpha': 1, 'beta': 2}
+
     @classmethod
     def deserialize(cls, jsonstr):
         #load from json to dict
@@ -10,7 +12,12 @@ class MySerializer(object):
         return cls(**d)
 
     def serialize(self):
+        t = {}
         #iterate over self.my_attrs
+
+        for d in self.my_attrs:
+           getattr(self, d)
         #get attrs, set into dictionary
+        t[] = val
         #return dumps
         return json.dumps(jsonstr)
